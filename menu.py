b653693428999f82a7fa691e8811160828acb432
Lazaro Yovanys Carabeo Vazquez <lazaro@lazaro.kali>
2024-06-27 19:15:28 -0400

    VideoDownloader

---
+++ menu.py
@@ -0,0 +1,111 @@
+from simple_term_menu import TerminalMenu
+from pytube import YouTube
+from tqdm import tqdm
+import time
+import os
+
+
+menu_entries = ["Search", "About ME", "Settings", "Exit"]
+terminal_menu = TerminalMenu(menu_entries)
+menu_entry_index = terminal_menu.show()
+
+
+match menu_entries[menu_entry_index]:
+    case "Search":
+        def on_progress_callback(stream, chunk, bytes_remaining):
+            progress_bar.update(len(chunk))
+
+
+        def download(n):
+            global progress_bar
+            stream = streams.get_by_itag(n)
+            total_size = stream.filesize
+            progress_bar = tqdm(total=total_size, unit='B', unit_scale=True)
+            yt.register_on_progress_callback(on_progress_callback)
+            
+            if stream:
+                stream.download()
+                progress_bar.close()
+                print("Download complete!")
+            else:
+                print("Not videos found with the specified format.")
+        
+
+        url = input("Enter URL: \n> ")
+        yt = YouTube(url)
+        format_list=[]
+        print(f'Is "{yt.title} by {yt.author}" the video you are looking for ? \n')
+        menu_yn = ["Yes", "No"]   
+        terminal_menu_yn = TerminalMenu(menu_yn)
+        menu_entry_index_yn = terminal_menu_yn.show()
+        
+        match menu_yn[menu_entry_index_yn]:
+            case "Yes": 
+       
+
+                streams = yt.streams.filter(file_extension='mp4')
+                menu_streams = []
+                for i, stream in enumerate(streams, start=1):
+                    string = f'{i}. Type: {stream.type} Resolution: {stream.resolution} Format: {stream.subtype} size: {stream.filesize_mb}'
+                    menu_streams.append(string)
+                    format_list.append(stream.itag)                    
+                menu_streams.append(f'{10}. Exit')    
+                    
+                    
+
+                        
+                while True:
+                    # Display menu
+                        terminal_menu_streams = TerminalMenu(menu_streams)
+                        menu_entry_index_streams = terminal_menu_streams.show()
+
+                    # Match user's selection
+                        if menu_entry_index_streams == 10:
+                            print('Exiting...')
+                            time.sleep(2)
+                            break
+                        elif 1 <= menu_entry_index_streams <= 9:
+                            # Download the selected format
+                            download(format_list[menu_entry_index_streams])
+                        else:
+                            print('Invalid option. Try again.')
+
+            case "No":
+                os.system('cls' if os.name == 'nt' else 'clear')
+                menu_entry_index = terminal_menu.show()
+
+                                            
+
+
+
+
+                    
+
+
+    case "About ME":
+        print("""⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣀⣤⣤⣶⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣶⣶⣶⣦⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀
+⠀⠀ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠉⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀
+⠀⠀ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠈⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀
+⠀⠀ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀
+⠀⠀ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⢀⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀
+⠀⠀ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⣠⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀
+⠀⠀ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢀⣠⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀ ⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀
+⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠟⠛⠛⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+""")
+        print("Youtube downloader v.0.1.0 \nDeveloped by Lazaro Yovanys Carabeo\n\n\n")
+        menu_entry_index = terminal_menu.show()
+
+
+
+    case "Exit":
+        exit()
+
+     
+
